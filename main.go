3793788def1c8ab7ddf18fd1b7e75ea57a98fb60
Rahul Saravanan <128274814+BlastBringer@users.noreply.github.com>
2025-11-11 16:35:49 +0530

    nov 11 2025

--- main.go
+++ main.go
@@ -1,21 +1,67 @@
-package main 
+package main  
+
 
 import (
-	"time"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"fmt"
-	"math/rand"
+
 )
 
-func main(){
-	ch := make(chan int)
+type Shop struct{
+	earnings int
+	start bool
+}
 
-	for i := 0; i < 3; i++{
-		go func(id int) {
-			time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
-			ch <- id
-		}(i)
+
+
+
+
+
+
+const (
+	screenWidth = 400
+	screenHeight = 400
+)
+
+type  Game struct{
+	shop Shop 
+	tick int 
+	earningSpeed int
+	counter int 
+}
+func (g *Game)Update() error{
+
+	if g.shop.start{
+		g.counter++
+		if g.counter >= g.earningSpeed {
+			g.shop.earnings++
+			g.counter = 0
+		}
 	}
+	return nil
+}
 
-	winner := <-ch 
-	fmt.Println("winner is", winner)
+func (g *Game)Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("Earnings : %d\n Tick : %d", g.shop.earnings, g.tick))
+}
+
+
+
+func(g *Game)Layout(outsideWidth, outsideHeight int) (int, int){
+	return screenWidth, screenHeight
+}
+
+
+
+
+
+func main() {
+	game := &Game{shop: Shop{earnings : 0, start : true} , earningSpeed: 60}
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Cat shop proto")
+	err := ebiten.RunGame(game); if err != nil{
+		fmt.Println("Error while runing the game! :", err)
+	}	
+
 }
