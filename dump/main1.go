c5b856136fd0da907d9dcb230a056788aab86c69
Rahul Saravanan <128274814+BlastBringer@users.noreply.github.com>
2025-11-21 18:54:45 +0530

    nov21

--- dump/main1.go
+++ dump/main1.go
@@ -1,67 +1,35 @@
-package main  
+package main 
 
 
-import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"fmt"
+import "fmt"
 
-)
-
-type Shop struct{
-	earnings int
-	start bool
-}
-
-
-
-
-
-
-
-const (
-	screenWidth = 400
-	screenHeight = 400
-)
-
-type  Game struct{
-	shop Shop 
-	tick int 
-	earningSpeed int
-	counter int 
-}
-func (g *Game)Update() error{
-
-	if g.shop.start{
-		g.counter++
-		if g.counter >= g.earningSpeed {
-			g.shop.earnings++
-			g.counter = 0
-		}
+func hello(name, language string) string{
+	if language == "es"{
+		return "Hola, " + name
 	}
-	return nil
-}
 
-func (g *Game)Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("Earnings : %d\n Tick : %d", g.shop.earnings, g.tick))
-}
+	if language == "fr"{
+		return "Bonjour, "+ name
+	}
 
-
-
-func(g *Game)Layout(outsideWidth, outsideHeight int) (int, int){
-	return screenWidth, screenHeight
+	return "Hello, " + name
 }
 
 
+//refactored
+func Hello(name, language string) string{
+	return fmt.Sprintf("%s , %s", greeting[language], name)
+}
+var greeting = map[string]string{
+	"es" : "Hola",
+	"fr" : "Bonjour",
+	"en" : "Hello",
+}
 
 
 
-func main() {
-	game := &Game{shop: Shop{earnings : 0, start : true} , earningSpeed: 60}
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Cat shop proto")
-	err := ebiten.RunGame(game); if err != nil{
-		fmt.Println("Error while runing the game! :", err)
-	}	
-
+func main(){
+	fmt.Println("hello world!")
+	fmt.Println(hello("rahul", "en"),
+	Hello("rahul", "fr"))
 }
