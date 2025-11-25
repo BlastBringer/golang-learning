5c599afd4f5d4ef5f093b6ecc4b54f07580fb82c
Rahul Saravanan <128274814+BlastBringer@users.noreply.github.com>
2025-11-25 14:08:47 +0530

    nov252025

--- testingfiles/structs/shapes.go
+++ testingfiles/structs/shapes.go
@@ -7,6 +7,15 @@ type Rectangle struct{
 	Height float64
 }
 
+type Shape interface{
+	Area() float64
+}
+
+type Triangle struct{
+	Base float64 
+	Height float64
+}
+
 type Circle struct{
 	Radius float64
 }
@@ -17,5 +26,9 @@ func (c Circle) Area() float64{
 
 func (r Rectangle)Area()float64{
 	return r.Height * r.Width
+}
+
+func(t Triangle)Area() float64{
+	return(t.Base * t.Height) * 0.5
 }
 
--- testingfiles/structs/shapes_test.go
+++ testingfiles/structs/shapes_test.go
@@ -4,24 +4,22 @@ import "testing"
 
 
 func TestArea(t *testing.T){
-	t.Run("rectangles", func(t *testing.T){
-		rectangle := Rectangle{12,6}
-		got := rectangle.Area()
+	areaTests := []struct{
+		name string
+		shape Shape
+		want float64
+	} {
+		{name : "rectangle", shape : Rectangle{12, 6}, want : 72.0},
+		{name : "Circle", shape : Circle{10},want: 314.1592653589793},
+		{name : "triangle", shape : Triangle{12, 6}, want : 36.0},
+	}
 
-		want := 72.0
-		if got != want{
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
-
-
-	t.Run("circles", func(t *testing.T){
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want{
-			t.Errorf("got %g want %g", got ,want)
-		}
-	})
+	for _, tt := range areaTests{
+		t.Run(tt.name, func(t *testing.T){
+			got := tt.shape.Area( )
+			if got != tt.want{
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
 }
