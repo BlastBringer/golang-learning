0217b4e28d1931aba4953f926c604bc339f27f02
Rahul Saravanan <128274814+BlastBringer@users.noreply.github.com>
2025-11-29 10:57:02 +0530

    game dev

--- go.mod
+++ go.mod
@@ -6,9 +6,12 @@ require (
 	github.com/ebitengine/gomobile v0.0.0-20250923094054-ea854a63cce1 // indirect
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
 	github.com/ebitengine/purego v0.9.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.9.2 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,38 +2,50 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"log"
 	_ "image/png"
+ "image/color"
 )
 
-var fishimg *ebiten.Image
+const (
+	gridSize = 40
+	screenH = 600
+	screenW = 800
+)
 
-func init(){
-	var err error 
-	fishimg, _, err = ebitenutil.NewImageFromFile("fish.png")
-	if err != nil{
-		 log.Fatal(err)
-	}
-}
 
 type Game struct{
-
+	camX float64
+	camY float64
 }
 
 func(g *Game) Update () error{
-	return nil
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {g.camX += 4}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {g.camX -= 4}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {g.camY += 4}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {g.camY -= 4}
+	return nil 
 }
 func (g *Game)Draw(screen *ebiten.Image){
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(0.75, 0.75)
+	screen.Fill(color.RGBA{20,20,20,255})
+	offX := int(g.camX) % gridSize 
+	offY := int(g.camY) % gridSize 
 
-	ebitenutil.DebugPrint(screen, "hello there ebiten!")
-	screen.DrawImage(fishimg, op)
+	for x := -offX; x < screenW; x += gridSize{
+		for y := -offY ; y < screenH; y += gridSize{
+			rect := ebiten.NewImage(gridSize-1, gridSize-1)
+			rect.Fill(color.RGBA{60,60,60,255})
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(x), float64(y))
+			screen.DrawImage(rect, op)
+		}
+	}
+
+
 }
 
 func (g *Game)Layout(outsideWidth , outsideHeight int) (int, int){
-	return 640, 480
+	return screenW, screenH
 }
 
 func main(){
